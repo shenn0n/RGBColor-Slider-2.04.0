42d1a9a7bbdb83c0d9b6ac0183e67d223ce49157
Alexandr Manzhosov <shenn0n4412@gmail.com>
2025-05-02 00:40:03 +0300

    add outlets and actions

--- RGBColor Slider 2.04.0/ViewController.swift
+++ RGBColor Slider 2.04.0/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
-
-
+    @IBAction func redSliderAction() {
+        
+    }
+    
+    @IBAction func greenSliderAction() {
+        
+    }
+    
+    @IBAction func blueSliderAction() {
+        
+    }
+    
 }
 
