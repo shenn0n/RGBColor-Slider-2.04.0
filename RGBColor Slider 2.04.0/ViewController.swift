e3f9a2dafd13ee91663faf441cb6db261b781e88
Alexandr Manzhosov <shenn0n4412@gmail.com>
2025-05-01 22:27:46 +0300

    init commit

---
+++ RGBColor Slider 2.04.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RGBColor Slider 2.04.0
+//
+//  Created by Александр Манжосов on 01.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
