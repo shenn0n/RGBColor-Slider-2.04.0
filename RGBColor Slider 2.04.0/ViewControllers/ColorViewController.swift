2cabdb4c026424bd482bdc4cac899c1b34e76014
Alexandr Manzhosov <shenn0n4412@gmail.com>
2025-05-07 17:07:26 +0300

    Color transfer between controllers

--- RGBColor Slider 2.04.0/ViewControllers/ColorViewController.swift
+++ RGBColor Slider 2.04.0/ViewControllers/ColorViewController.swift
@@ -7,8 +7,21 @@
 
 import UIKit
 
+protocol ComposeColorViewControllerDelegate: AnyObject {
+    func setBackgroundColor(_ color: UIColor)
+}
+
 final class ColorViewController: UIViewController {
     
-    
-    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let composeColorVC = segue.destination as? ComposeColorViewController
+        composeColorVC?.color = view.backgroundColor
+        composeColorVC?.delegate = self
+    }
+}
+
+extension ColorViewController: ComposeColorViewControllerDelegate {
+    func setBackgroundColor(_ color: UIColor) {
+        view.backgroundColor = color
+    }
 }
--- RGBColor Slider 2.04.0/ViewControllers/ComposeColorViewController.swift
+++ RGBColor Slider 2.04.0/ViewControllers/ComposeColorViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 final class ComposeColorViewController: UIViewController {
     
+    // MARK: - IB Outlets
     @IBOutlet var colorView: UIView!
     
     @IBOutlet var redSlider: UISlider!
@@ -19,48 +20,55 @@ final class ComposeColorViewController: UIViewController {
     @IBOutlet var greenLabel: UILabel!
     @IBOutlet var blueLabel: UILabel!
     
+    // MARK: - Public Properties
+    var color: UIColor!
+    weak var delegate: ComposeColorViewControllerDelegate?
+    
+    // MARK: - Overrides Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupSlidersFromColor(from: color)
+        setViewColor()
+        
         redSlider.minimumValue = 0
         redSlider.maximumValue = 1
-        redSlider.value = 0.05
         
         greenSlider.minimumValue = 0
         greenSlider.maximumValue = 1
-        greenSlider.value = 0.27
         
         blueSlider.minimumValue = 0
         blueSlider.maximumValue = 1
-        blueSlider.value = 0.49
         
         redLabel.text = String(format: "%.2f", redSlider.value)
         greenLabel.text = String(format: "%.2f", greenSlider.value)
         blueLabel.text = String(format: "%.2f", blueSlider.value)
         
-        setViewColor()
     }
     
-    @IBAction func redSliderAction() {
-        redLabel.text = String(format: "%.2f", redSlider.value)
+    // MARK: - IB Actions
+    @IBAction func sliderAction(_ sender: UISlider) {
+        if sender == redSlider {
+            redLabel.text = String(format: "%.2f", redSlider.value)
+        } else if sender == greenSlider {
+            greenLabel.text = String(format: "%.2f", greenSlider.value)
+        } else if sender == blueSlider {
+            blueLabel.text = String(format: "%.2f", blueSlider.value)
+        }
         setViewColor()
     }
-    
-    @IBAction func greenSliderAction() {
-        greenLabel.text = String(format: "%.2f", greenSlider.value)
-        setViewColor()
-    }
-    
-    @IBAction func blueSliderAction() {
-        blueLabel.text = String(format: "%.2f", blueSlider.value)
-        setViewColor()
-    }
-    
+
     @IBAction func doneButtonTapped() {
+        delegate?.setBackgroundColor(colorView.backgroundColor ?? UIColor.white)
         dismiss(animated: true)
     }
     
-    private func setViewColor() {
+}
+
+// MARK: - setupInitialValues
+private extension ComposeColorViewController {
+    
+    func setViewColor() {
         colorView.backgroundColor = UIColor(
             red: CGFloat(redSlider.value),
             green: CGFloat(greenSlider.value),
@@ -68,5 +76,20 @@ final class ComposeColorViewController: UIViewController {
             alpha: 1.0
         )
     }
+    
+    func setupSlidersFromColor(from color: UIColor) {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        var alpha: CGFloat = 0
+        
+        if color.getRed(&red, green: &green, blue: &blue, alpha: &alpha) {
+            redSlider.value = Float(red)
+            greenSlider.value = Float(green)
+            blueSlider.value = Float(blue)
+        } else {
+            print("Не удалось получить RGB-компоненты из переданного цвета")
+        }
+    }
+    
 }
-
